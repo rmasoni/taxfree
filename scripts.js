f4a5cdda1ef77c4ca34fce19ee75319811547302
Rafael Masoni <rafaelmasoni@gmail.com>
2016-06-09 14:24:17 -0300

    Allow user to run calculation by pressing return in a field

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-$("#calculate").on("click", function() {
+$("form").on("submit", function() {
   var calc = parseFloat($("#wish").val()) * parseFloat($("#tax").val()) / 100 + parseFloat($("#wish").val());
   if (isNaN(calc)) {
     $("#result").val("Insira um número.");
   } else {
     $("#result").val("R$ " + calc.toFixed(2));
   };
+  return false;
 });
 
 });
